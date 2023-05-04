bd2238cf7b9323f992e1349f197172354ea9bbe4
unknown0verlord <unknown0verlordxbox@gmail.com>
2023-05-04 00:49:20 -0400

    Got some variables movin'

--- src/main.js
+++ src/main.js
@@ -3,51 +3,22 @@ import "./main.css"
 
 import Towers from "./towers";
 
-const myContext = React.createContext();
-
 class Main extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            dataFromChild: null,
+
             patch: "v1.1.0",
-            potatoes: 0,
-            PpS: 0,
-            potatoUp: 1,
-            spudSpitter: 0,
-            spudSpitterCost: 10,
 
-            farmer: 0,
-            farmerCost: 50,
-            farmerPpSadd: 1,
-
-            farm: 0,
-            farmCost: 300,
-            farmPpSadd: 5,
-
-            factory: 0,
-            factoryCost: 1000,
-            factoryPpSadd: 20,
-
-            district: 0,
-            districtCost: 5000,
-            districtPpSadd: 50,
-
-            convention: 0,
-            conventionCost: 20000,
-            conventionPpSadd: 100,
-
-            merchandise: 0,
-            merchandiseCost: 100000,
-            merchandisePpSadd: 500,
-
-            cult: 0,
-            cultCost: 666666,
-            cultPpSadd: 3000,
-
-            sponsorships: 0,
-            sponsorshipsCost: 1234567,
-            sponsorshipsPpSadd: 10000,
+            mainStats: {
+                potatoes: 0,
+                PpS: 0,
+                potatoUp: 1,
+            },
+            
+            
                 
             biggerYields: 0,
             moreFields: 0,
@@ -58,40 +29,47 @@ class Main extends Component {
             potatoSkinSacrifice: 0,
             moreBrands: 0
         }
+        this.handleDataFromChild = this.handleDataFromChild.bind(this);
     }
 
     clickFirstButton = () => {
-        var potatoes = this.state.potatoes + this.state.potatoUp;
-        this.setState({potatoes: potatoes});
+        var mainStats = this.state.mainStats
+        mainStats.potatoes = this.state.mainStats.potatoes + this.state.mainStats.potatoUp;
+        this.setState({mainStats: mainStats});
     }
 
-    progressBar = () => {
-        var elem = document.getElementById("progressBar");   
-        var width = 1;
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-            } else {
-                width++; 
-                elem.style.width = width + '%'; 
-            }
-        }
-    }
+    // progressBar = () => {
+    //     var elem = document.getElementById("progressBar");   
+    //     var width = 1;
+    //     var id = setInterval(frame, 10);
+    //     function frame() {
+    //         if (width >= 100) {
+    //             clearInterval(id);
+    //         } else {
+    //             width++; 
+    //             elem.style.width = width + '%'; 
+    //         }
+    //     }
+    // }
     
-    progress = setInterval(this.progressBar, 1000);
+    // progress = setInterval(this.progressBar, 1000);
+
+    handleDataFromChild(data) {
+        this.setState({ dataFromChild: data });
+      }
 
     render() {
+        var value1 = this.state.mainStats.potatoes;
         return(
             <div>
                 {/* Main Div with Potatoes and PpS */}
                 <div id="showVar">
-                    <p id="varCount">Potatoes: {this.state.potatoes}</p>
-                    <p id="PpSCount">PpS: {this.state.PpS}</p>
+                    <p id="varCount">Potatoes: {this.state.mainStats.potatoes}</p>
+                    <p id="PpSCount">PpS: {this.state.mainStats.PpS}</p>
             
                     {/* Progress Bar */}
-                    <div class="progress-grey">
-                        <div class="progress-green" id="progressBar"></div>
+                    <div className="progress-grey">
+                        <div className="progress-green" id="progressBar"></div>
                     </div>
                 </div>
 
@@ -101,7 +79,7 @@ class Main extends Component {
                 {/* Make a Potato Button */}
                 <button type="button" onClick={this.clickFirstButton}>Make a Potato!</button>
 
-                <Towers state={this.state} />
+                <Towers potatoes={value1} />
             </div>
             
             
--- src/towers.js
+++ src/towers.js
@@ -6,80 +6,112 @@ import "./main.css";
 class Towers extends Component {
     constructor(props) {
         super(props);
+        var testvalue = props.potatoes;
+        this.state = {
+            potatoes: testvalue,
+            
+            towers: {
+                spudSpitter: 0,
+                spudSpitterCost: 10,
 
-        this.state = props.state;
+                farmer: 0,
+                farmerCost: 50,
+                farmerPpSadd: 1,
+
+                farm: 0,
+                farmCost: 300,
+                farmPpSadd: 5,
+
+                factory: 0,
+                factoryCost: 1000,
+                factoryPpSadd: 20,
+
+                district: 0,
+                districtCost: 5000,
+                districtPpSadd: 50,
+
+                convention: 0,
+                conventionCost: 20000,
+                conventionPpSadd: 100,
+
+                merchandise: 0,
+                merchandiseCost: 100000,
+                merchandisePpSadd: 500,
+
+                cult: 0,
+                cultCost: 666666,
+                cultPpSadd: 3000,
+
+                sponsorships: 0,
+                sponsorshipsCost: 1234567,
+                sponsorshipsPpSadd: 10000,
+            }
+            
+        };
+        this.test = this.test.bind(this);
     }
 
 
 
 towers = (number) => {
     if (number == 1) {
-        if (this.game.potatoes >= this.game.spudSpitterCost) {
-            this.game.spudSpitter += 1;
-            this.game.potatoes -= this.game.spudSpitterCost;
-            this.game.spudSpitterCost = Math.floor(10 * (1.25 ** this.game.spudSpitter));
-            this.game.potatoUp += 1;
-
-            if (this.game.potatoes < this.game.spudSpitterCost) {
-                document.getElementById("spudSpitter").style.backgroundColor = '#006600';
-            }
-
-            document.getElementById("varCount").innerHTML = "Potatoes: " + this.game.potatoes.toLocaleString();
-            document.getElementById("spudSpitterCount").innerHTML = "Spud Spitters: " + this.game.spudSpitter.toLocaleString();
-            document.getElementById("spudSpitterText").innerHTML = "Cost: " + this.game.spudSpitterCost.toLocaleString();
-            console.log(this.game.spudSpitter);
+        if (this.state.potatoes >= this.state.spudSpitterCost) {
+            
             }
     } else if (number == 2) {
-        if (this.game.potatoes >= this.game.farmerCost) {
-            this.game.farmer += 1;
-            this.game.potatoes -= this.game.farmerCost;
-            this.game.farmerCost = Math.floor(50 * (1.15 ** this.game.farmer));
-            this.game.PpS += this.game.farmerPpSadd;
-
-            if (this.game.potatoes < this.game.farmerCost) {
-                document.getElementById("potatoFarmer").style.backgroundColor = '#006600';
-            }
-
-            document.getElementById("varCount").innerHTML = "Potatoes: " + this.game.potatoes.toLocaleString();
-            document.getElementById("PpSCount").innerHTML = "PpS: " + this.game.PpS.toLocaleString();
-            document.getElementById("farmerCount").innerHTML = "Potato Farmers: " + this.game.farmer.toLocaleString();
-            document.getElementById("farmerText").innerHTML = "Cost: " + this.game.farmerCost.toLocaleString();
-            console.log(this.game.farmer);
+        if (this.state.potatoes >= this.state.farmerCost) {
+            
         }
     } else if (number == 3) {
-        if (this.game.potatoes >= this.game.farmCost) {
+        if (this.state.potatoes >= this.state.farmCost) {
             
         }
     } else if (number == 4) {
-        if (this.game.potatoes >= this.game.factoryCost) {
+        if (this.state.potatoes >= this.state.factoryCost) {
             
         }
     } else if (number == 5) {
-        if (this.game.potatoes >= this.game.districtCost) {
+        if (this.state.potatoes >= this.state.districtCost) {
             
         }
     } else if (number == 6) {
-        if (this.game.potatoes >= this.game.conventionCost) {
+        if (this.state.potatoes >= this.state.conventionCost) {
             
         }
     } else if (number == 7) {
-        if (this.game.potatoes >= this.game.merchandiseCost) {
+        if (this.state.potatoes >= this.state.merchandiseCost) {
             
         }
     }  else if (number == 8) {
-        if (this.game.potatoes >= this.game.cultCost) {
+        if (this.state.potatoes >= this.state.cultCost) {
             
         }
     }  else if (number == 9) {
-        if (this.game.potatoes >= this.game.sponsorshipsCost) {
+        if (this.state.potatoes >= this.state.sponsorshipsCost) {
             
         }
     }
 }
 
+test = () => {
+    // this.props.onData(this.state);
+}
+
+chooseStyle = () => {
+    var potatoes = this.props.potatoes;
+    console.log(potatoes);
+    if (potatoes >= this.state.towers.spudSpitterCost) {
+        return "#33cc33"
+    } else {
+        return "#006600"
+    }
+    
+}
+
 
 
     render() {
+        {this.state.potatoes = this.props.potatoes}
         return(
             <div>
                 
@@ -88,74 +120,74 @@ towers = (number) => {
                 <br />
 
                 {/* Spud Spitter Tower Button */}
-                <button class="towers" id="spudSpitter" type="button" onClick={this.towers(1)}>
+                <button className="towers" id="spudSpitter" type="button" onClick={this.test} style={{backgroundColor: "#33cc33"}}>
                     <p>Spud Spitter</p>
-                    <p class="towerText" id="spudSpitterText">Cost: 10</p>
-                    <p class="towerDesc">Makes more potatoes per click.</p>
+                    <p className="towerText" id="spudSpitterText">Cost: {this.state.towers.spudSpitterCost}</p>
+                    <p className="towerDesc">Makes more potatoes per click.</p>
                 </button>
 
                 <br />
 
                 {/* Potato Farmer Tower Button */}
-                <button class="towers" id="potatoFarmer" type="Button" onClick={this.towers(2)}>
+                <button className="towers" id="potatoFarmer" type="Button" onClick={this.towers(2)}>
                     <p>Potato Farmer</p>
-                    <p class="towerText" id="farmerText">Cost: 50</p>
-                    <p class="towerDesc" id="farmerDesc"></p>
+                    <p className="towerText" id="farmerText">Cost: {this.state.towers.farmerCost}</p>
+                    <p className="towerDesc" id="farmerDesc">This Honest Man gives you his potatoes &#40;{this.state.farmerPpSadd} PpS&#41;</p>
                 </button>
 
                 {/* Potato Farm Tower Button */}
-                <button class="towers" id="potatoFarm" type="Button" onClick={this.towers(3)}>
+                <button className="towers" id="potatoFarm" type="Button" onClick={this.towers(3)}>
                     <p>Potato Farm</p>
-                    <p class="towerText" id="farmText">Cost: 300</p>
-                    <p class="towerDesc" id="farmDesc"></p>
+                    <p className="towerText" id="farmText">Cost: 300</p>
+                    <p className="towerDesc" id="farmDesc">A whole farm dedicated to your potatoes. &#40;{this.state.farmPpSadd} PpS&#41;</p>
                 </button>
 
                 <br />
 
                 {/* Potato Factory Tower Button */}
-                <button class="towers" id="potatoFactory" type="Button" onClick={this.towers(4)}>
+                <button className="towers" id="potatoFactory" type="Button" onClick={this.towers(4)}>
                     <p>Potato Factory</p>
-                    <p class="towerText" id="factoryText">Cost: 1,000</p>
-                    <p class="towerDesc" id="factoryDesc"></p>
+                    <p className="towerText" id="factoryText">Cost: 1,000</p>
+                    <p className="towerDesc" id="factoryDesc">Streamline Production in Potato Factories. &#40;{this.state.factoryPpSadd} PpS&#41;</p>
                 </button>
 
                 {/* Industrial District Tower Button */}
-                <button class="towers" id="industrialDistrict" type="Button" onClick={this.towers(5)}>
+                <button className="towers" id="industrialDistrict" type="Button" onClick={this.towers(5)}>
                     <p>Industrial Disrict</p>
-                    <p class="towerText" id="districtText">Cost: 5,000</p>
-                    <p class="towerDesc" id="districtDesc"></p>
+                    <p className="towerText" id="districtText">Cost: 5,000</p>
+                    <p className="towerDesc" id="districtDesc">An entire district of factories dedicated to Potatoes.  &#40;{this.state.districtPpSadd} PpS&#41;</p>
                 </button>
 
                 <br />
 
                 {/* Potato Convention Tower Button */}
-                <button class="towers" id="potatoConvention" type="Button" onClick={this.towers(6)}>
+                <button className="towers" id="potatoConvention" type="Button" onClick={this.towers(6)}>
                     <p>Potato Convention</p>
-                    <p class="towerText" id="conventionText">Cost: 20,000</p>
-                    <p class="towerDesc" id="conventionDesc"></p>
+                    <p className="towerText" id="conventionText">Cost: 20,000</p>
+                    <p className="towerDesc" id="conventionDesc">Host a convention for potatoes - "Potato-Con"! &#40;{this.state.conventionPpSadd} PpS&#41;</p>
                 </button>
 
                 {/* Potato Merchandise Tower Button */}
-                <button class="towers" id="potatoMerchandise" type="Button" onClick={this.towers(7)}>
+                <button className="towers" id="potatoMerchandise" type="Button" onClick={this.towers(7)}>
                     <p>Potato Merchandise</p>
-                    <p class="towerText" id="merchandiseText">Cost: 100,000</p>
-                    <p class="towerDesc" id="merchandiseDesc"></p>
+                    <p className="towerText" id="merchandiseText">Cost: 100,000</p>
+                    <p className="towerDesc" id="merchandiseDesc">Sell Merchandise with Potato Branding. &#40;{this.state.merchandisePpSadd} PpS&#41;</p>
                 </button>
 
                 <br />
 
                 {/* Potato Cult Tower Button */}
-                <button class="towers" id="potatoCult" type="Button" onClick={this.towers(8)}>
+                <button className="towers" id="potatoCult" type="Button" onClick={this.towers(8)}>
                     <p>Potato Cult</p>
-                    <p class="towerText" id="cultText">Cost: 666,666</p>
-                    <p class="towerDesc" id="cultDesc"></p>
+                    <p className="towerText" id="cultText">Cost: 666,666</p>
+                    <p className="towerDesc" id="cultDesc">Start Cults worshipping Potatoes. &#40;{this.state.cultPpSadd} PpS&#41;</p>
                 </button>
         
                 {/* Potato Sponsorships Tower Button */}
-                <button class="towers" id="potatoSponsorships" type="Button" onClick={this.towers(9)}>
+                <button className="towers" id="potatoSponsorships" type="Button" onClick={this.towers(9)}>
                     <p>Potato Sponsorships</p>
-                    <p class="towerText" id="sponsorshipsText">Cost: 1,234,567</p>
-                    <p class="towerDesc" id="sponsorshipsDesc"></p>
+                    <p className="towerText" id="sponsorshipsText">Cost: 1,234,567</p>
+                    <p className="towerDesc" id="sponsorshipsDesc">Sponsor Events with Potatoes. &#40;{this.state.sponsorshipsPpSadd} PpS&#41;</p>
                 </button>
             </div>
         )
